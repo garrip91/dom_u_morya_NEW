376d80f9daaeb9e5b7a31cc7a1b054b71f2b9465
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-05-06 23:14:59 +0300

    Summary

--- src/dom_u_morya/settings.py
+++ src/dom_u_morya/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'dom_u_morya.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], # BASE_DIR / 'templates'
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
