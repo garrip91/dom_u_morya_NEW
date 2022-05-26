fc95a5eccb79c3bddf7a21a8dae5c202ec9a7682
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-05-26 21:39:19 +0300

    Update settings.py

--- src/dom_u_morya/settings.py
+++ src/dom_u_morya/settings.py
@@ -143,3 +143,11 @@ CACHES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
